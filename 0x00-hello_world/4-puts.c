9ef79a9a085bc0cb00c0c1abee8269163fe40505
Dereje Desta <ddesta777@gmail.com>
2021-08-26 17:01:26 +0300

    Modiefied 4 and 5 to conform with betty's style guide

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
+/* main */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
