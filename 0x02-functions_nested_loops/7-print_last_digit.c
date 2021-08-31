f292643f684493f506b47c60aca4afe7a7e2252e
Dereje Desta <ddesta777@gmail.com>
2021-08-31 15:23:58 +0300

    added 6, 7 and 8

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - value of last digit
+ * @n : int
+ * Return: return the value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int last = n % 10;
+
+	_putchar(last);
+	return (last);
+}
