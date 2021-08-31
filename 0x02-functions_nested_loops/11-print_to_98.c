de561b9031b814f4f7309f870739375e0af2ed7d
Dereje Desta <ddesta777@gmail.com>
2021-08-31 23:20:37 +0300

    added 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h"
+
+/**
+* print_to_98 - prints upto 98
+* @n: from n
+*/
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i < 98; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+
+	for (i = n; i > 98; i--)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+
+	printf("98\n");
+}
