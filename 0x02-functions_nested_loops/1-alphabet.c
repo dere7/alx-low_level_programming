b5c6cbda5c7a5d00e0b49b8ae982b97d9668fefe
Dereje Desta <ddesta777@gmail.com>
2021-08-31 13:34:30 +0300

    added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "holberton.h"
+
+/**
+ *  print_alphabet - prints alphabets
+ *  Return: void
+ */
+void print_alphabet(void)
+{
+	for(int i='a';i<='z';i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
