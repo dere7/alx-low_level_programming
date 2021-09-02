21abcce3140d8e276a3c4cf0960ff1e5180f3b91
Dereje Desta <ddesta777@gmail.com>
2021-09-02 16:51:56 +0300

    Added 8, 9 ad 10

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 1; j <= 100; j++)
+	{
+		int by3 = j % 3 == 0;
+		int by5 = j % 5 == 0;
+
+		if (by3 && by5)
+			printf("FizzBuzz");
+		else if (by3)
+			printf("Fizz");
+		else if (by5)
+			printf("Buzz");
+		else
+			printf("%d", j);
+		putchar(' ');
+	}
+
+	return (0);
+}
