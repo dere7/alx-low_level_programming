36e50386500aa45dce5a81fd79139227ac1e224d
Dereje Desta <ddesta777@gmail.com>
2021-09-02 16:17:11 +0300

    minor fixes

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,7 +9,7 @@ void more_numbers(void)
 
 	for (j = 0; j < 10; j++)
 	{
-		for (i = 0; i < 14; i++)
+		for (i = 0; i <= 14; i++)
 		{
 			int ones = i % 10;
 			int tens = i / 10;
@@ -19,7 +19,8 @@ void more_numbers(void)
 
 			_putchar('0' + ones);
 		}
+
+		_putchar('\n');
 	}
 
-	_putchar('\n');
 }
