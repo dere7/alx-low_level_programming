32b686678d8353da8dfd16a8566763ada27d8f90
Dereje Desta <ddesta777@gmail.com>
2021-09-06 18:00:03 +0300

    Minor fixes

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,7 +6,7 @@
  */
 void puts_half(char *s)
 {
-	int i = 0, len = 0, n;
+	int i = 0, len = 0;
 
 	while (s[i] != '\0')
 	{
@@ -14,9 +14,7 @@ void puts_half(char *s)
 		i++;
 	}
 
-	n = len % 2 == 0 ? len / 2 : (len - 1) / 2;
-
-	for (i = n; i < len; i++)
+	for (i = len / 2; i < len; i++)
 	{
 		_putchar(s[i]);
 	}
