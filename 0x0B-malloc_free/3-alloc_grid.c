3b36115be75cef011b6074b79cdb2c53c1bf3659
Dereje Desta <ddesta777@gmail.com>
2021-09-21 15:27:38 +0300

    minor fixes

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	if (width == 0 || height == 0)
 		return (NULL);
 
-	ch = malloc(sizeof(int) * height);
+	ch = malloc(sizeof(int *) * height);
 
 	for (i = 0; i < height; i++)
 	{
