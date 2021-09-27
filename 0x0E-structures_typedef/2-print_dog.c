00d48f1ce800195af6b2a1bfefae5f10b949856c
Dereje Desta <ddesta777@gmail.com>
2021-09-27 21:20:43 +0300

    minor fixe

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,13 +7,10 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
-
-	printf("Name: %s\n", (d->name != NULL ? d->name : "(nil)"));
-	if (d->age == 0)
-		printf("Age: (nil)\n");
-	else
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name != NULL ? d->name : "(nil)"));
 		printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", (d->owner != NULL ? d->owner : "(nil)"));
+		printf("Owner: %s\n", (d->owner != NULL ? d->owner : "(nil)"));
+	}
 }
--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,14 +9,21 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i = 0;
 	dog_t *d = malloc(sizeof(struct dog));
 
 	if (d == NULL)
 		return (NULL);
 
-	d->name = name;
+	while (name[i] != '\0')
+		d->name[i] = name[i];
+	d->name[i] = '\0';
+
+	i = 0;
+	while (owner[i] != '\0')
+		d->owner[i] = owner[i];
+	d->owner[i] = '\0';
 	d->age = age;
-	d->owner = owner;
 
 	return (d);
 }
