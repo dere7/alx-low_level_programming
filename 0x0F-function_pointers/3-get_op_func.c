76cef7dbcafa8dcc8f2d2f4e0129004c26ef6099
Dereje Desta <ddesta777@gmail.com>
2021-09-30 09:14:43 +0300

    minor fixes for 0 and 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,10 +20,11 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (i++ < 5)
+	while (i < 5)
 	{
 		if (!strcmp(s, ops[i].op))
 			return (ops[i].f);
+		i++;
 	}
 
 	return (NULL);
