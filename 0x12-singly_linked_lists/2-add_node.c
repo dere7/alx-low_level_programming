cd8f010ab8f7a2a0e79ca5163e105eee55cf747f
Dereje Desta <ddesta777@gmail.com>
2021-10-08 13:12:25 +0300

    minor fixes: followed style guidlines

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,6 +2,12 @@
 #include <string.h>
 #include <stdlib.h>
 
+/**
+ * add_node - add new node at the begging of list
+ * @head: head of list to be added
+ * @str: string of node
+ * Return: Pointer to newly created node
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
