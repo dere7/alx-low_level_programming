984e4ef87498349498138636e1a569530e49a2f4
Dereje Desta <ddesta777@gmail.com>
2021-10-08 12:43:21 +0300

    Initial commit for singly linked project

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,10 +14,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(arg, n);
 	for (i = 0; i < n; i++)
 	{
-		if (i != (n - 1))
-			printf("%d%s", va_arg(arg, int), separator);
-		else
-			printf("%d", va_arg(arg, int));
+		printf("%d", va_arg(arg, int));
+		if (i != (n - 1) && separator)
+			printf("%s", separator);
 	}
 	printf("\n");
+	va_end(arg);
 }
