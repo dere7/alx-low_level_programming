0c69fa9e16ac0e9716c7a0448bf81bfa292c9603
Dereje Desta <ddesta777@gmail.com>
2022-02-10 01:16:29 +0300

    Minor fixes

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,16 +7,23 @@ contains function that calculates island perimeter
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid
     grid: 2d list of integer - 1 land zone & 0 water zone"""
+
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if grid[i - 1][j] == 0:
+                try:
+                    if grid[i - 1][j] == 0 and grid[i + 1][j] == 0\
+                       and grid[i][j - 1] == 0 and grid[i][j + 1] == 0:
+                        continue
+                except IndexError:
+                    pass
+                if i == 0 or grid[i - 1][j] == 0:
                     perimeter += 1
-                if grid[i + 1][j] == 0:
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
-                if grid[i][j - 1] == 0:
+                if j == 0 or grid[i][j - 1] == 0:
                     perimeter += 1
-                if grid[i][j + 1] == 0:
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
     return perimeter
